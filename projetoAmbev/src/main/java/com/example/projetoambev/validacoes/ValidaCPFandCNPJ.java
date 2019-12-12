427eb16f4af96a992e90cc20c38eaf731e80efbf
Bruno Gimenez <bruno.roberto.costa.gimenez@gmail.com>
2019-12-12 07:59:12 -0300

    Update ValidaCPFandCNPJ.java

--- projetoAmbev/src/main/java/com/example/projetoambev/validacoes/ValidaCPFandCNPJ.java
+++ projetoAmbev/src/main/java/com/example/projetoambev/validacoes/ValidaCPFandCNPJ.java
@@ -5,7 +5,7 @@ public class ValidaCPFandCNPJ
 
      {
         private static final int[] pesoCPF = {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-        private static final int[] pesoCNPJ = {6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        private static final int[] pesoCNPJ = {22, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
 
         public static boolean isValid(String cpfCnpj) {
             return (isValidCPF(cpfCnpj) || isValidCNPJ(cpfCnpj));
